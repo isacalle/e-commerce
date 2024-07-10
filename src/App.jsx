11f1b5a44e1486d845d739a9b1fe996fb920bf2f
isacalle <isabelacalle.dev@gmail.com>
2024-07-09 21:00:50 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Titulo from "./components/Titulo"
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+         <p>
+        Hola mundo
+      </p>
+      <Titulo />
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+const Titulo = () => {
+    return(
+        <div> 
+            <h1> Hola Hola Hola </h1>
+        </div>
+    )
+}
+
+export default Titulo
